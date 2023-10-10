2ea4bde88f41a2b95beb6bfda1f92a48c01ef765
Samuelj58 <144460795+Samuelj58@users.noreply.github.com>
2023-10-10 10:20:35 -0400

    ss

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const Borscht = {
+  Name: "Borscht",
+  Orgin: "Russia/Ukraine",
+  Eaten: "Hot, Cold",
+};
+const CroqueMadame = {
+  Name: "Croque Madame",
+  Orgin: "France",
+  Eaten: "Hot",
+};
+const Kimchi = {
+  Name: "Kimchi",
+  Orgin: "Korea",
+  Eaten: "Hot, Cold",
+};
+const Souvlaki = {
+  Name: "Souvlaki",
+  Orgin: "Greece",
+  Eaten: "Hot",
+};
+const Schweinshaxe = {
+  Name: "Schweinshaxe",
+  Orgin: "Germany",
+  Eaten: "Hot",
+};
+const Kitfo = {
+  Name: "Kitfo",
+  Orgin: "Ethiopia",
+  Eaten: "Hot, Cold",
+};
+
+console.log();
